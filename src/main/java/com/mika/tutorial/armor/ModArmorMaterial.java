bc00f62b8794534caa9d9936a12fba831038014a
Mika Lightsmith <mika.f.lightsmith@gmail.com>
2020-08-06 10:26:18 -0700

    custom armor

---
+++ src/main/java/com/mika/tutorial/armor/ModArmorMaterial.java
@@ -0,0 +1,83 @@
+package com.mika.tutorial.armor;
+
+import com.mika.tutorial.Tutorial;
+import com.mika.tutorial.util.RegistryHandler;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterial implements IArmorMaterial {
+
+    // ruby_layer_1 ruby_layer_2
+    RUBY(Tutorial.MOD_ID + ":ruby", 39, new int[]{
+            3, // helmet
+            6, // leggings
+            8, // chestplate
+            3 // boots
+    }, 29, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2, () -> {
+        return Ingredient.fromItems(RegistryHandler.RUBY.get());
+    });
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[]{11, 16, 15, 13};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability,
+                     SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial) {
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageReductionAmountArray = damageReductionAmountArray;
+        this.enchantability = enchantability;
+        this.toughness = toughness;
+        this.repairMaterial = repairMaterial;
+        this.soundEvent = soundEvent;
+    }
+
+
+    @Override
+    public int getDurability(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    @Override
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent() {
+        return soundEvent;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.get();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+    }
--- src/main/java/com/mika/tutorial/util/RegistryHandler.java
+++ src/main/java/com/mika/tutorial/util/RegistryHandler.java
@@ -1,14 +1,14 @@
 package com.mika.tutorial.util;
 
 import com.mika.tutorial.Tutorial;
+import com.mika.tutorial.armor.ModArmorMaterial;
 import com.mika.tutorial.blocks.BlockItemBase;
 import com.mika.tutorial.blocks.RubyBlock;
 import com.mika.tutorial.items.ItemBase;
 import com.mika.tutorial.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.PickaxeItem;
-import net.minecraft.item.SwordItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -29,9 +29,25 @@ public class RegistryHandler {
     // Tools
     public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
             new SwordItem(ModItemTier.RUBY, 2, 0.0F, new Item.Properties().group(Tutorial.TAB)));
-
     public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
             new PickaxeItem(ModItemTier.RUBY, 0, -1.0F, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
+            new ShovelItem(ModItemTier.RUBY, 0.4f, -2.0F, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
+            new AxeItem(ModItemTier.RUBY, 6, -2.0F, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () ->
+            new HoeItem(ModItemTier.RUBY, 0, new Item.Properties().group(Tutorial.TAB)));
+
+    // Armor
+    public static final RegistryObject<ArmorItem> RUBY_HELMET = ITEMS.register("ruby_helmet", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.HEAD, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_CHESTPLATE = ITEMS.register("ruby_chestplate", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.CHEST, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_LEGGINGS = ITEMS.register("ruby_laginges", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.LEGS, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_BOOTS = ITEMS.register("ruby_boots", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.FEET, new Item.Properties().group(Tutorial.TAB)));
+
 
     // Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
